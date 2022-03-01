b524b06d96d96b02d2ef313579dfe05d6d8ef171
Zach <shaw.zacht@gmail.com>
2022-02-28 23:26:42 -0800

    commit tree 17335c7a872a42cc156def1a074a65da61d39d91

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.querySelector('#main').remove()
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.textContent = "Zach is the champion"
+
